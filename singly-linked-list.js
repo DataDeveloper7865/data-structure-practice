a286b20cde20c3dd41d30904511957b1caa39f99
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-10 19:30:50 -0700

    added methods

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -71,6 +71,85 @@ class SinglyLinkedList {
 
     }
 
+    //shift -> pop the head!
+    shift() {
+        if (!this.head) return undefined;
+        let curHead = this.head;
+        this.head = this.head.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return curHead.val
+    }
+
+    //unfshit() -> add to beginning
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this
+    }
+
+    //get takes index or position and gives 
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
+        let counter = 0
+        let current = this.head;
+        while(counter != index) {
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+
+    //set an item
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index == this.length) return !!this.push(val);
+        if (index == 0) return !!this.unshift(val);
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    //remove
+    remove(index) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        let previousNode = this.get(index -1);
+        let removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+
+        return removed
+    }
+
 }
 
 let list = new SinglyLinkedList()
