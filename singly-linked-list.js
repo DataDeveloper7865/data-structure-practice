795266e7d349448226532e7effcf0c1d660127df
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-10 19:55:32 -0700

    added reverse

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -150,6 +150,22 @@ class SinglyLinkedList {
         return removed
     }
 
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+
 }
 
 let list = new SinglyLinkedList()
