8d7182e6629ee931a282e631ba412194f90291af
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-13 14:08:45 -0700

    added remove methods

--- graph.js
+++ graph.js
@@ -14,4 +14,17 @@ class Graph {
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(el => el == v2)
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(el => el == v1)
+    }
+
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
 }
