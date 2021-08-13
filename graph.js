cf67d18d4891a4e0df563aceefb965e86106ea32
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-13 12:54:34 -0700

    added vertex and edge methods

--- graph.js
+++ graph.js
@@ -5,4 +5,13 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+
+    addVertex(name) {
+        this.adjacencyList[name] = [];
+    }
+
+    addEdge(start, end) {
+        this.adjacencyList[start].push(end);
+        this.adjacencyList[end].push(start);
+    }
 }
