7a2baaceef60437e6d8768fd604d2b90a75e2658
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-08-17 12:14:59 -0700

    binary Search shorthand

--- binary-search.js
+++ binary-search.js
@@ -22,3 +22,27 @@ function binarySearch(arr, elem) {
     }
 
 }
+
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((end - start) / 2);
+    while(arr[middle] !== elem && start <= end) {
+        if (arr[middle] > elem) end = middle - 1;
+        else start = middle + 1;
+        middle = Math.floor((end - start) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
+
+function binarySearch(arr, elem) {
+    let start = 0;
+    let end = arr.length - 1;
+    let middle = Math.floor((end - start) / 2);
+    while (arr[middle] !== elem && start <= end) {
+        if (arr[middle] > elem) end = middle -1;
+        else start = middle + 1;
+        middle = Math.floor((end - start) / 2);
+    }
+    return arr[middle] === elem ? middle : -1;
+}
